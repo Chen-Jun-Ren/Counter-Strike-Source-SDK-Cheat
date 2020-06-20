94934d8eabc7072a573425eb9fc40bffa9a38c9c
kevin5881413 <kevin5881413kevin@gmail.com>
2020-06-21 03:09:24 +0800

    new Message

--- CSSCreateInterface/BonesESP.cpp
+++ CSSCreateInterface/BonesESP.cpp
@@ -50,6 +50,7 @@ void doBonesESP(IDirect3DDevice9* pDevice)
 				//		Tools::DrawString(Bone2D_Pos_A.x, Bone2D_Pos_A.y, 255, 255, 255, "%d", i);
 				//	}
 				//}
+
 				for (int i = 0; i <= (IM_ARRAYSIZE(Bone_ID_A) - 1); i++)//sizeof(Bone_ID_A) = 72
 				{
 					if (BaseEntity->GetTeamNumber() == LocalPlayer->GetTeamNumber() && Controller::BonesESP_check_Team)//------Team------//
